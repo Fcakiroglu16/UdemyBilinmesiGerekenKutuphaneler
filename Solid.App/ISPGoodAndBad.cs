8587bc3a69e7e7cb7eb268341fc571e012eec2da
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2022-10-08 15:30:50 +0300

    Interface SegregationPrinciple

---
+++ Solid.App/ISPGoodAndBad.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid.App.ISPGoodAndBad
+{
+
+    // Class Library  Read Impl
+    // Class Library  Create/Update/Delete Impl
+
+    public class Product{
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+    public interface IProductRepository
+    {
+        List<Product> GetList();
+        Product GetById(int id);
+
+        Product Create(Product p);
+        Product Update(Product p);
+        Product Delete(Product p);
+
+
+
+    }
+
+
+}
